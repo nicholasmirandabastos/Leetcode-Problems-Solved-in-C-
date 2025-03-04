6c8dd8f2261a7cf4951fcef3b56594525cba23f5
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-03-04 17:31:45 -0300

    Binary Search

---
+++ 01. EASY/704 - Binary Search/BinarySearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    public class BinarySearch
+    {
+        public static int BSearch(int[] nums, int target)
+        {
+            int lenArr = nums.Count();
+
+            int lowPos = 0;
+            int highPos = lenArr - 1; // garantir que o máximo é dentro do array
+
+            while (lowPos < highPos)
+            {
+                int middle = (int)((lowPos + highPos) / 2); // pegar o meio
+
+                // se o elemento do meio for o alvo, retorna sua posição
+                if (nums[middle] == target)
+                    return middle;
+
+                // se o elemento do meio for menor que o alvo, descarta essa metade e ajusta o limite
+                if (nums[middle] < target)
+                    lowPos = middle + 1;
+                else
+                    // caso o elemento do meio for maior que o alvo, já descarta a metade e ajusta o limite
+                    highPos = middle;
+            }
+
+            // retorna -1 se o alvo não for encontrado
+            return -1;
+        }
+    }
+
+}
---
+++ 01. EASY/704 - Binary Search/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Binary_Search;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bem-vindo ao programa de Busca Binária!");
+
+        Console.Write("Digite os números ordenados separados por espaço: ");
+        string? input = Console.ReadLine();
+        int[] numbers;
+
+        try
+        {
+            numbers = input.Split(' ').Select(int.Parse).OrderBy(n => n).ToArray();
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Entrada inválida. Certifique-se de digitar apenas números.");
+            return;
+        }
+
+        Console.Write("Digite o número a ser buscado: ");
+        if (!int.TryParse(Console.ReadLine(), out int target))
+        {
+            Console.WriteLine("Entrada inválida. Certifique-se de digitar um número válido.");
+            return;
+        }
+
+        int? result = BinarySearch.BSearch(numbers, target);
+
+        if (result != -1)
+            Console.Write($"Elemento {target} encontrado no índice {result}.");
+        else
+            Console.Write($"Elemento {target} não encontrado.");
+    }
+}
