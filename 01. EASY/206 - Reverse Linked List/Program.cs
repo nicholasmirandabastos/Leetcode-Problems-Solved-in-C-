75d540dd5529ce13b779b0f9523e989acb13c1e2
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-03-11 21:44:27 -0300

    206 - Reverse Linked List

---
+++ 01. EASY/206 - Reverse Linked List/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ReverseLinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bem-vindo ao programa de Reverse Linked List!");
+
+            Console.Write("Digite os números da lista ligados por espaço: ");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input)) // valida entrada vazia ou nula
+            {
+                Console.WriteLine("Entrada inválida!");
+                return;
+            }
+
+            string[] values = input.Split(' ');
+            ListNode head = null, current = null;
+
+            foreach (string value in values)
+            {
+                if (int.TryParse(value, out int num)) // valida se é um número inteiro
+                {
+                    ListNode newNode = new ListNode(num);
+                    if (head == null)
+                    {
+                        head = newNode;
+                        current = head;
+                    }
+                    else
+                    {
+                        current.next = newNode;
+                        current = newNode;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Valor inválido ignorado: {value}");
+                }
+            }
+
+            ReverseLinkedList solution = new ReverseLinkedList();
+            ListNode reversedHead = solution.ReverseList(head);
+
+            Console.Write("Lista invertida: ");
+            while (reversedHead != null)
+            {
+                Console.Write(reversedHead.val + " ");
+                reversedHead = reversedHead.next;
+            }
+
+        }
+    }
+
+}
---
+++ 01. EASY/206 - Reverse Linked List/ReverseLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseLinkedList
+{
+    public class ListNode
+    {
+        public int val; // valor do nó na linked list
+        public ListNode next; // referência para o próximo nó da lista
+
+        // construtor da classe ListNode que inicializa o valor e a referência para o próximo nó
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val; // define o valor do nó
+            this.next = next; // define a referência para o próximo nó
+        }
+    }
+    public class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode newList = null; // inicializa a nova lista invertida como nula
+
+            while (head != null) // percorre a lista original até que head seja nulo
+            {
+                ListNode nextNode = head.next; // armazena a referência para o próximo nó da lista original
+                head.next = newList; // faz o nó atual apontar para a nova lista (invertendo a ligação)
+                newList = head; // move o nó atual para ser o primeiro da nova lista
+                head = nextNode; // avança para o próximo nó na lista original
+            }
+
+            return newList; 
+        }
+    }
+}
