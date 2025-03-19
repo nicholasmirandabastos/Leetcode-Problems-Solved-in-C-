b1875cda426dca676dd95ce10440dd502a07fd44
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-03-18 21:48:15 -0300

    876 - Middle of the Linked List

---
+++ 01. EASY/876 - Middle of the Linked List/MiddleLinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middle_of_the_Linked_List
+{
+    public class ListNode
+    {
+        public int val; // valor do nó na linked list
+        public ListNode next; // referência para o próximo nó da lista
+
+        // construtor da classe ListNode que inicializa o valor e a referência para o próximo nó
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class MiddleLinkedList
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode ahead = head; // inicializar ponteiro
+
+            while (ahead != null && ahead.next != null)
+            {
+                ahead = ahead.next.next; // ahead move-se duas posições à frente
+                head = head.next; // head move uma posição à frente
+            }
+            return head; // quando ahead chega ao final da lista indica que head estará no meio
+        }
+    }
+}
---
+++ 01. EASY/876 - Middle of the Linked List/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Middle_of_the_Linked_List
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bem-vindo ao programa de Middle of the Linked List!");
+
+            Console.Write("Digite os números da lista separados por espaço: ");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input)) // valida entrada vazia ou nula
+            {
+                Console.WriteLine("Entrada inválida!");
+                return;
+            }
+
+            string[] values = input.Split(' ');
+            ListNode head = null, current = null;
+
+            foreach (string value in values)
+            {
+                if (int.TryParse(value, out int num)) // valida se é um número inteiro
+                {
+                    ListNode newNode = new ListNode(num);
+                    if (head == null)
+                    {
+                        head = newNode;
+                        current = head;
+                    }
+                    else
+                    {
+                        current.next = newNode;
+                        current = newNode;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Valor inválido ignorado: {value}");
+                }
+            }
+
+            MiddleLinkedList MiddleNode = new MiddleLinkedList();
+            ListNode middleList = MiddleNode.MiddleNode(head);
+
+            Console.Write("Meio da Lista: ");
+            while (middleList != null)
+            {
+                Console.Write(middleList.val + " ");
+                middleList = middleList.next;
+            }
+
+        }
+    }
+
+}
