b9500f2497782393005eef2e1397d294ae1fd947
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-03-19 22:14:42 -0300

    141 - Linked List Cycle

---
+++ 01. EASY/141 - Linked List Cycle/LinkedListCycle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_Cycle
+{
+    public class ListNode
+    {
+        public int val; // valor do nó na linked list
+        public ListNode next; // referência para o próximo nó da lista
+
+        // construtor da classe ListNode que inicializa o valor e a referência para o próximo nó
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null) // enquanto não forem nulos, indica que a lista ainda não foi terminada
+            {
+                slow = slow.next; // move um nó por vez
+                fast = fast.next.next; // move dois nós por vez
+                if (slow == fast) // se os ponteiros se encontram, significa haver um ciclo na lsita
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 01. EASY/141 - Linked List Cycle/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Linked_List_Cycle
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bem-vindo ao programa de Linked List Cycle!");
+
+            Console.Write("Digite os números da lista separados por espaço: ");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Entrada inválida!");
+                return;
+            }
+
+            string[] values = input.Split(' ');
+            List<ListNode> nodes = new List<ListNode>();
+            ListNode head = null, current = null;
+
+            foreach (string value in values)
+            {
+                if (int.TryParse(value, out int num))
+                {
+                    ListNode newNode = new ListNode(num);
+                    nodes.Add(newNode);
+
+                    if (head == null)
+                    {
+                        head = newNode;
+                        current = head;
+                    }
+                    else
+                    {
+                        current.next = newNode;
+                        current = newNode;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Valor inválido ignorado: {value}");
+                }
+            }
+
+            if (head == null)
+            {
+                Console.WriteLine("Nenhum número válido foi inserido!");
+                return;
+            }
+
+            Console.Write("Digite a posição do nó para criar um ciclo: ");
+            string posInput = Console.ReadLine();
+
+            if (int.TryParse(posInput, out int pos) && pos >= 0 && pos < nodes.Count)
+            {
+                current.next = nodes[pos];
+                Console.WriteLine($"Ciclo criado conectando o último nó ao nó na posição {pos}.");
+            }
+
+            LinkedListCycle cycleChecker = new LinkedListCycle();
+            bool hasCycle = cycleChecker.HasCycle(head);
+
+            Console.WriteLine(hasCycle ? "A lista contém ciclo!" : "A lista não contém ciclo.");
+        }
+    }
+
+}
