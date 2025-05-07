169304b095b4107aa9ad863f75250ca717d78931
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-05-06 22:43:26 -0300

    1342 - Number of Steps to Reduce a Number to Zero

---
+++ 01. EASY/1342 - Number of Steps to Reduce a Number to Zero/NumberOfSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_Steps_to_Reduce_a_Number_to_Zero;
+
+    public class NumberOfStepsToReduce
+{
+
+    public int NumberOfSteps(int num)
+    {
+        int steps = 0;
+        while (num > 0)
+        {
+            if ((num & 1) == 1) // verifica se num é ímpar ou par
+            {
+                num -= 1; // subtraí 1 devido bit menos significativo
+            }
+            else
+            {
+                num >>= 1; // divide por 2 usando deslocamento de bits
+            }
+            steps += 1;
+
+        }
+        return steps;
+    }
+}
---
+++ 01. EASY/1342 - Number of Steps to Reduce a Number to Zero/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Number_of_Steps_to_Reduce_a_Number_to_Zero;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+
+        while (true)
+        {
+            Console.Write("Digite um número inteiro não negativo: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out number) && number >= 0)
+            {
+                break;
+            }
+
+            Console.WriteLine("Entrada inválida. Tente novamente.");
+        }
+
+        NumberOfStepsToReduce calculator = new NumberOfStepsToReduce();
+        int steps = calculator.NumberOfSteps(number);
+
+        Console.WriteLine($"Número de passos para reduzir {number} a zero: {steps}");
+    }
+}
