ad3846c551f7e4529c3ffef7a0d0aa6d8112b4a2
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-05-07 21:50:24 -0300

    338 - Counting Bits

---
+++ 01. EASY/338 - Counting Bits/CountBits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Bits;
+public class Counting_Bits
+{
+    public int[] CountBits(int n)
+    {
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i <= n; i++)
+        {
+            if ((i & 1) == 1) // verifica se num é ímpar ou par
+            {
+                ans[i] = ans[i / 2] + 1; // para ímpares, adiciona 1 ao valor da metade
+            }
+            else
+            {
+                ans[i] = ans[i / 2]; // para números pares, herda o valor da metade
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ 01. EASY/338 - Counting Bits/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Counting_Bits;
+
+class Program
+{
+    public static void Main()
+    {
+        int input;
+        while (true)
+        {
+            Console.Write("Digite um número inteiro positivo: ");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out input) && input >= 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida.");
+            }
+        }
+
+        Counting_Bits cb = new Counting_Bits();
+        int[] result = cb.CountBits(input);
+
+        Console.WriteLine("Quantidade de bits 1 para cada número até " + input + ":");
+        for (int i = 0; i <= input; i++)
+        {
+            Console.WriteLine($"Número {i} => {result[i]} bits 1");
+        }
+    }
+}
