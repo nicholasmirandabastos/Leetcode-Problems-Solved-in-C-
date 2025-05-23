2cde355e052e252c12503dfd8a57fe69a2003ef6
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-05-22 21:47:32 -0300

    155 - Min Stack

---
+++ 02. MEDIUM/155 - Min Stack/MinStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinStack
+{
+    public class MinStack
+    {
+        // Pilha principal que armazena todos os valores
+        private Stack<int> stack;
+        // Pilha auxiliar que mantém o menor valor a cada operação de push
+        private Stack<int> minStack;
+
+        public MinStack()
+        {
+            stack = new Stack<int>();
+            minStack = new Stack<int>();
+        }
+
+        public void Push(int val)
+        {
+            stack.Push(val); // Adiciona o valor à pilha principal
+
+            // Se a pilha de mínimos não estiver vazia, comparamos o novo valor com o menor atual
+            if (minStack.Count > 0)
+            {
+                val = System.Math.Min(minStack.Peek(), val); // Pegamos o menor entre o valor atual e o do topo da minStack
+            }
+
+            minStack.Push(val); // Armazena o novo menor valor na pilha de mínimos
+        }
+
+        // Remove o valor do topo da pilha
+        public void Pop()
+        {
+            if (stack.Count > 0) // Verifica se há elementos na pilha antes de remover
+            {
+                stack.Pop();     // Remove o topo da pilha principal
+                minStack.Pop();  // Remove o topo da pilha de mínimos (que acompanha a principal)
+            }
+        }
+
+        // Retorna o valor do topo da pilha
+        public int Top()
+        {
+            return stack.Peek(); // Retorna o último valor inserido na pilha
+        }
+
+        // Retorna o menor valor da pilha
+        public int GetMin()
+        {
+            return minStack.Peek(); // Retorna o topo da pilha de mínimos
+        }
+    }
+}
---
+++ 02. MEDIUM/155 - Min Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace MinStack;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao programa de MinStack!");
+        // Instancia a MinStack
+        MinStack minStack = new MinStack();
+
+        // Executa as operações
+        minStack.Push(-2);
+        minStack.Push(0);
+        minStack.Push(-3);
+        Console.WriteLine(minStack.GetMin()); // Deve imprimir -3
+        minStack.Pop();
+        Console.WriteLine(minStack.Top());    // Deve imprimir 0
+        Console.WriteLine(minStack.GetMin()); // Deve imprimir -2
+    }
+}
