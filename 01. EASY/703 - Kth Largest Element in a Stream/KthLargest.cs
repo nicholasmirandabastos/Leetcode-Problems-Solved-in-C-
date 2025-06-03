c148b6bdc0a6fb1c70f922f5083fce9697c07953
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-06-02 22:32:21 -0300

    703 - Kth Largest Element in a Stream

---
+++ 01. EASY/703 - Kth Largest Element in a Stream/KthLargest.cs
@@ -0,0 +1,34 @@
+﻿namespace Kth_Largest_Element_in_a_Stream
+{
+    public class KthLargest
+    {
+        private PriorityQueue<int, int> minHeap; 
+        private int k; 
+
+        public KthLargest(int k, IList<int> nums)
+        {
+            this.k = k;
+
+            // cria o heap mínimo com os elementos existentes
+            minHeap = new PriorityQueue<int, int>();
+            foreach (var num in nums)
+            {
+                Add(num);
+            }
+        }
+
+        public int Add(int val)
+        {
+            minHeap.Enqueue(val, val);
+
+            // Se a heap tiver mais de k elementos, remove o menor (topo da heap)
+            if (minHeap.Count > k)
+            {
+                minHeap.Dequeue();
+            }
+
+            return minHeap.Peek();
+        }
+    }
+
+}
---
+++ 01. EASY/703 - Kth Largest Element in a Stream/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kth_Largest_Element_in_a_Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de k: ");
+            int k = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite os números iniciais separados por espaço: ");
+            var entradaInicial = Console.ReadLine().Split(' ');
+            var numerosIniciais = new List<int>();
+            foreach (var item in entradaInicial)
+            {
+                if (int.TryParse(item, out int num))
+                {
+                    numerosIniciais.Add(num);
+                }
+            }
+
+            var kth = new KthLargest(k, numerosIniciais);
+            var resultados = new List<string>();
+            resultados.Add("null");
+
+            Console.WriteLine("Digite os valores para adicionar (um por vez). Digite 'sair' para encerrar:");
+
+            while (true)
+            {
+                Console.Write("Adicionar: ");
+                var entrada = Console.ReadLine();
+
+                if (entrada.ToLower() == "sair") break;
+
+                if (int.TryParse(entrada, out int valor))
+                {
+                    int resultado = kth.Add(valor);
+                    resultados.Add(resultado.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido.");
+                }
+            }
+
+            Console.WriteLine($"Saída: [{string.Join(", ", resultados)}]");
+        }
+    }
+}
