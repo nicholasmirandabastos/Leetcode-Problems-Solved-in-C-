f5264d3ff4602c700fa606bdf0d4c9d1637aee47
nicholasmirandabastos <nickjasonsp@gmail.com>
2025-06-11 20:28:32 -0300

    70 - Climbing Stairs

---
+++ 01. EASY/70 - Climbing Stairs/ClimbingStairs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Climbing_Stairs
+{
+    class ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1) // se nf or igual a 1, já retorna 1
+            {
+                return 1;
+            }
+
+            int prev = 1, curr = 1; // inicializa os dois primeiros valores
+
+            for (int i = 2; i <= n; i++) // loop do índice 2 até o n2
+            {
+                int temp = curr; // salva o valor atual de curr em temp
+                curr = prev + curr; // calcula o próximo da sequência
+                prev = temp; // atualiza o prev com o valor anterior de curr, na próxima iteração prev será F(n-1)
+            }
+
+            return curr;
+        }
+    }
+
+}
---
+++ 01. EASY/70 - Climbing Stairs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Climbing_Stairs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao programa de Calculador de Formas de Subir!");
+        
+        int steps;
+
+        // Loop até o usuário inserir um número inteiro válido
+        while (true)
+        {
+            Console.Write("Digite o número de degraus (inteiro): ");
+            string input = Console.ReadLine();
+
+            // Tenta converter o valor para inteiro
+            if (int.TryParse(input, out steps))
+            {
+                break; // Sai do loop se a conversão for bem-sucedida
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+            }
+        }
+
+        ClimbingStairs solver = new ClimbingStairs();
+
+        int result = solver.ClimbStairs(steps);
+
+        Console.WriteLine($"Número de formas de subir {steps} degraus: {result}");
+    }
+}
